3391b7f0d14c0a69637d4e573f42f03270f2a7dc
Mich <17795210+121Unicorns@users.noreply.github.com>
2022-10-16 02:19:03 -0700

    Commit Solution for Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * Existence is pain to a Meeseeks
+ * This script prints "Programming is like building a multilingual puzzle"
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
