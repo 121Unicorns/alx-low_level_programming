c095187ac3ce15e93971de2c3432a21f44b299d5
Mich <17795210+121Unicorns@users.noreply.github.com>
2022-10-16 02:59:31 -0700

    Commit Solution for Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
 *
 * Description: prints "Programming is like building a multilingual puzzle"
 *
-* Return: Always 0 (Success)
+* Return: Is 0 (Success)
 */
 
 int main(void) 
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0); 
+puts("\"Programming is like building a multilingual puzzle");
+return (0); 
 }
