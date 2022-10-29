106e4ed5a1917620798be11823f9e43a1f47762e
Mich <mich@unicorn>
2022-10-29 19:47:48 +0300

    Commit Solution for Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strcat - Concatenates two strings.
+* @src: String that gets added to dest
+* @dest: String to append to
+* Return: Is 0.
+*/
+char *_strcat(char *dest, char *src)
+{
+int a = 0;
+int b = 0;
+while (dest[a] != '\0')
+a++;
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+b++;
+a++;
+}
+return (dest);
+}
