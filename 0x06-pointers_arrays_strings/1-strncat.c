1a2110a274f4adab1b3b4d002fa42b44e23a1076
Mich <mich@unicorn>
2022-10-29 19:54:25 +0300

    Commit Solution for Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,13 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int a = 0;
-int b = 0;
-while (dest[a] != '\0')
-a++;
-while (src[b] != '\0' && b < n)
+int index = 0;
+int dest_len = 0;
+while (dest[index++])
+dest_len++;
+for(index=0; src[index] && index <n; index++)
 {
-dest[a] = src[b];
-b++;
-a++;
+dest[dest_len] = src[index];
 }
 return (dest);
 }
